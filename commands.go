a0716fa793e12ed5ba5bef07535c55da80b37b4d
Tamelien <Tamelien@protonmail.com>
2026-03-20 00:49:18 +0100

    feat: add users command to list all users with current user

--- commands.go
+++ commands.go
@@ -89,3 +89,19 @@ func handlerReset(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerGetUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		fmt.Printf("* %s", user.Name)
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Print(" (current)")
+		}
+		fmt.Print("\n")
+	}
+
+	return nil
+}
